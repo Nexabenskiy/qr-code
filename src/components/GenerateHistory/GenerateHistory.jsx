999602abb10ee7355583001bdc356fd7d682de50
VasileV <nexabenskiy@yandex.ru>
2025-06-02 14:55:33 +0300

    font-size and border-bottom

--- src/components/GenerateHistory/GenerateHistory.jsx
+++ src/components/GenerateHistory/GenerateHistory.jsx
@@ -9,8 +9,11 @@ export const GenerateHistory = () => {
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 20 }}>
       {data.map((item, index) => {
         return (
-          <div key={`${index}_${item}`} style={{ display: 'flex', alignAtems: 'center', justifyContent: 'center', gap: 15, width: '100%' }}>
-            <span style={{ fontSize: 40 }}>{item}</span>
+          <div
+            key={`${index}_${item}`}
+            style={{ display: 'flex', flexDirection: 'column', alignAtems: 'center', justifyContent: 'center', gap: 15, width: '100%', borderBottom: 1, borderBottomStyle: 'solid' }}
+          >
+            <span style={{ fontSize: 24 }}>{item}</span>
 
             <QRCode size={80} style={{ height: 'auto', maxWidth: 80, width: '100%' }} value={item} viewBox={`0 0 256 256`} />
           </div>
--- src/components/ScanHistory/ScanHistory.jsx
+++ src/components/ScanHistory/ScanHistory.jsx
@@ -11,8 +11,11 @@ export const ScanHistory = () => {
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 20 }}>
       {data.map((item, index) => {
         return (
-          <div key={`${item}_${index}`} style={{ display: 'flex', alignAtems: 'center', justifyContent: 'center', gap: 15, width: '100%' }}>
-            <p style={{ fontSize: 40 }}>{item}</p>
+          <div
+            key={`${item}_${index}`}
+            style={{ display: 'flex', flexDirection: 'column', alignAtems: 'center', justifyContent: 'center', gap: 15, width: '100%', borderBottom: 1, borderBottomStyle: 'solid' }}
+          >
+            <p style={{ fontSize: 24 }}>{item}</p>
             <QRCode size={80} style={{ height: 'auto', maxWidth: 80, width: '100%' }} value={item} viewBox={`0 0 256 256`} />
           </div>
         )
