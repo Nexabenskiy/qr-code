52ffecd374750166d1e9e240e502c046d162f1bd
VasileV <nexabenskiy@yandex.ru>
2025-06-02 15:15:21 +0300

    styles

--- src/components/ScanHistory/ScanHistory.jsx
+++ src/components/ScanHistory/ScanHistory.jsx
@@ -8,7 +8,7 @@ export const ScanHistory = () => {
   console.log(data)
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 20, marginTop: 270 }}>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 20, marginTop: 230 }}>
       {data.map((item, index) => {
         return (
           <div
